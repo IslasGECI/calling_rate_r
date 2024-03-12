0c8634549c0425c30cb6a9abd863cddaa22f8489
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-12 11:43:03 -0700

    ♻️  Refactor

--- R/filter_by_site.R
+++ R/filter_by_site.R
@@ -1,3 +1,4 @@
+#' @export
 filter_by_site <- function(monthly_data, sitio) {
   monthly_data |> dplyr::filter(Sitio == sitio)
 }
--- R/sort_months_in_rows.R
+++ R/sort_months_in_rows.R
@@ -1,3 +1,4 @@
+#' @export
 sort_months_in_rows <- function(messy_rows) {
   order_months <- c("November", "December", "January", "February", "March", "April", "May")
   messy_rows |> dplyr::arrange(factor(temporada, levels = order_months))
