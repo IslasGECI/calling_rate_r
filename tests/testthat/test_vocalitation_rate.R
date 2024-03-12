bd8abc2c5074ce57731e56d224ee467a83944215
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-12 11:59:17 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_vocalitation_rate.R
+++ tests/testthat/test_vocalitation_rate.R
@@ -1,6 +1,6 @@
 describe("Vocalization rate", {
   interest_values <- c(21, 32, 43, 54, 65)
-  column_of_interest <- tibble(interest_values)
+  column_of_interest <- tibble::tibble(interest_values)
   it("calculates the rate calling", {
     expected <- 4.3
     obtained <- calculate_rate_vocalization(column_of_interest)
