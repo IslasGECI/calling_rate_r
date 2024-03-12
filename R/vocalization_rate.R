b25688e3599de7b3b2bb156042b61467d42ac124
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-12 13:24:18 -0700

    ♻️  Refactor

--- R/vocalization_rate.R
+++ R/vocalization_rate.R
@@ -1,3 +1,4 @@
+#' @export
 calculate_rate_vocalization <- function(calls) {
   minutes_of_record <- 10
   mean(calls, na.rm = TRUE) / minutes_of_record
--- tests/testthat/test_vocalitation_rate.R
+++ tests/testthat/test_vocalitation_rate.R
@@ -1,9 +1,8 @@
 describe("Vocalization rate", {
   interest_values <- c(21, 32, 43, 54, 65)
-  column_of_interest <- tibble::tibble(interest_values)
   it("calculates the rate calling", {
     expected <- 4.3
-    obtained <- calculate_rate_vocalization(column_of_interest)
+    obtained <- calculate_rate_vocalization(interest_values)
     expect_equal(expected, obtained)
   })
 })
