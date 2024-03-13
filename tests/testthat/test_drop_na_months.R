f9474073f9243342949c96fbc48c0ba76b3b6368
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-13 09:32:39 -0700

    🛑🧪 Fail tests

---
+++ tests/testthat/test_drop_na_months.R
@@ -0,0 +1,10 @@
+describe("drop_months_with_na", {
+  data_with_na <- c(1, NA, 1)
+  regular_data <- c(10, 10, 10)
+  dataframe_with_na <- tibble::tibble("Ene" = data_with_na, "Feb" = data_with_na, "Mar" = regular_data)
+  it("Drop Ene and Feb", {
+    obtained <- drop_months_with_na(dataframe_with_na)
+    expected_ncols <- 1
+    expect_equal(length(ncol(obtained)), expected_ncols)
+  })
+})
