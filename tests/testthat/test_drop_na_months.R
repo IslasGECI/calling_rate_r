ddf81fddcffd55d02db38dd62279aeb3c7096050
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-13 09:40:52 -0700

    ♻️  Refactor

--- tests/testthat/test_drop_na_months.R
+++ tests/testthat/test_drop_na_months.R
@@ -4,15 +4,13 @@ describe("drop_months_with_na", {
   dataframe_with_na <- tibble::tibble("Ene" = data_with_na, "Feb" = data_with_na, "Mar" = regular_data)
   it("Drop Ene and Feb", {
     obtained <- drop_months_with_na(dataframe_with_na)
-    print(obtained)
     expected_ncols <- 1
     expect_equal(ncol(obtained), expected_ncols)
   })
   it("Drop month full of NAs (Ene)", {
     dataframe_with_na <- tibble::tibble("Ene" = NA, "Feb" = regular_data, "Mar" = regular_data)
     obtained <- drop_months_with_na(dataframe_with_na)
-    print(obtained)
-    expected_ncols <- 1
+    expected_ncols <- 2
     expect_equal(ncol(obtained), expected_ncols)
   })
 })
