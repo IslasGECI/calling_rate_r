1b9006f4489f0959801a859574b185b09c72bf4b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-20 13:32:25 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,3 +1,4 @@
+#' @export
 write_filtered_by_island <- function(options) {
   input_data <- readr::read_csv(options[["data_path"]])
   island <- options[["island"]]
--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -2,7 +2,7 @@ describe("Cli to filter by island", {
   it("Filter Socorro", {
     data_path <- "/workdir/tests/data/recordings_for_tests.csv"
     island <- "Socorro"
-    options <- list("data_path" = data_path, "island" = island)
+    options <- list("data_path" = data_path, "island" = island, "output_path" = "/workdir/tests/filtered_by_island.csv")
     write_filtered_by_island(options)
   })
 })
