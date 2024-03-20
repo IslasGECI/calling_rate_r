66c94d13f54d5712c5210ca11c758a5cdc9dce17
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-20 12:45:19 -0700

    ✅ Pass tests

---
+++ R/cli.R
@@ -0,0 +1,7 @@
+write_filtered_by_island <- function(options) {
+  input_data <- readr::read_csv(options[["data_path"]])
+  island <- options[["island"]]
+  input_data |>
+    filter_by_island(island) |>
+    readr::write_csv(options[["output_path"]])
+}
