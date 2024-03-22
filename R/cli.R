23e816e79d51061471814644c2d148b12d5d00de
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-22 14:13:56 -0700

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -10,9 +10,9 @@ write_filtered_by_island <- function(options) {
 
 #' @export
 write_dropped_off_recorder <- function(options) {
-  input_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
-  recorder_to_drop <- options[["scenarios"]]
+  input_data <- readr::read_csv(options[["data-path"]], show_col_types = FALSE)
+  recorder_to_drop <- options[["recorder-name"]]
   input_data |>
     drop_by_recorder(recorder_to_drop) |>
-    readr::write_csv(options[["output_path"]])
+    readr::write_csv(options[["output-path"]])
 }
