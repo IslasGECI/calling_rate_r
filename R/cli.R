0b54b4f82af8503429cb5c04121556da652941ae
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-22 11:32:16 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,6 +1,6 @@
 #' @export
 write_filtered_by_island <- function(options) {
-  input_data <- readr::read_csv(options[["data_path"]])
+  input_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
   island <- options[["island"]]
   input_data |>
     filter_by_island(island) |>
