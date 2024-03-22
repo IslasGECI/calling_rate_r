7609058404aeb5498a1ddfe31b854d960530ecd2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-22 11:38:09 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -6,3 +6,12 @@ describe("Cli to filter by island", {
     write_filtered_by_island(options)
   })
 })
+
+describe("Cli to drop by recorder", {
+  it("Filter Socorro2", {
+    data_path <- "/workdir/tests/data/recordings_for_tests.csv"
+    recorder_to_drop <- "Socorro2"
+    options <- list("data_path" = data_path, "recorder_name" = recorder_to_drop, "output_path" = "/workdir/tests/dropped_recorder.csv")
+    write_dropped_off_recorder(options)
+  })
+})
