f0ca8483560d9f67858344bdf0c86b30c030bada
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-28 21:33:17 +0000

    ♻️  Refactor

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -2,8 +2,12 @@ describe("Cli to filter by island", {
   it("Filter Socorro", {
     data_path <- "/workdir/tests/data/recordings_for_tests.csv"
     island <- "Socorro"
-    options <- list("data_path" = data_path, "island" = island, "output_path" = "/workdir/tests/filtered_by_island.csv")
+    output_path <- "/workdir/tests/filtered_by_island.csv"
+    options <- list("data_path" = data_path, "island" = island, "output_path" = output_path)
+    testtools::if_exist_remove(output_path)
     write_filtered_by_island(options)
+    expect_true(testtools::exist_output_file(output_path))
+    testtools::if_exist_remove(output_path)
   })
 })
 
@@ -11,7 +15,11 @@ describe("Cli to drop by recorder", {
   it("Filter Socorro2", {
     data_path <- "/workdir/tests/data/recordings_for_tests.csv"
     recorder_to_drop <- "Socorro2"
-    options <- list("data-path" = data_path, "recorder-name" = recorder_to_drop, "output-path" = "/workdir/tests/dropped_recorder.csv")
+    output_path <- "/workdir/tests/dropped_recorder.csv"
+    options <- list("data-path" = data_path, "recorder-name" = recorder_to_drop, "output-path" = output_path)
+    testtools::if_exist_remove(output_path)
     write_dropped_off_recorder(options)
+    expect_true(testtools::exist_output_file(output_path))
+    testtools::if_exist_remove(output_path)
   })
 })
